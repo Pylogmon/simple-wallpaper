6ade91a16f42c0f1456cb97211a9a74725acf1f2
ambition_echo <ambition_echo@outlook.com>
2023-01-14 15:51:57 +0800

    chore: 替换默认图标

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,20 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu};
+
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "退出");
+    let tray_menu = SystemTrayMenu::new().add_item(quit);
+    let system_tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .system_tray(system_tray)
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|_app_handle, event| match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
